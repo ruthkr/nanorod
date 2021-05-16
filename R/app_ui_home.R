aa8223e5e27411c63500010ace1ec67b60c8f581
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-05-16 23:32:59 +0100

    Added app_body_home() for About page

---
+++ R/app_ui_home.R
@@ -0,0 +1,27 @@
+#' The app home
+#'
+#' @import shiny
+#' @noRd
+app_body_home <- function() {
+  tabPanel(
+    title = "About",
+    # icon = icon("info-circle"),
+    id = "tab-home",
+    value = "home",
+    col_12(
+      # class = "home-page",
+      h2("About this tool"),
+      includeMarkdown(
+        app_sys("app/www", "app_body.md")
+      ),
+
+      actionButton(
+        inputId = "btn_github",
+        label = "Source code",
+        class = "btn-primary",
+        icon = icon("github"),
+        onclick = "window.open('https://github.com/ruthkr/nanorod', '_blank')"
+      )
+    )
+  )
+}
