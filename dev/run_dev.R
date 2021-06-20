ef2058994760b959d85bb95fa4b67e9de3061e9f
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-06-20 22:35:31 +0100

    Reload {devtools} and recompile CSS file with golem::run_dev()

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -4,9 +4,13 @@ options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mo
 # Detach all loaded packages and clean your environment
 golem::detach_all_attached()
 # rm(list=ls(all.names = TRUE))
+library(devtools)
 
 # Document and reload your package
 golem::document_and_reload()
+
+# Recompile CSS file
+source(here::here("inst/app/www/sass", "compile_sass.R"))
 
 # Run the application
 run_app()
