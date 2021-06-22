964f0b10a74a8c7f9bfab55c3bab8dc5c6f6840c
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-06-23 00:00:20 +0100

    Added render_datatable_justified() and render_datatable_justified_nopage() functions; improved DT dom

--- R/mod_nanorod_stats_utils_tables.R
+++ R/mod_nanorod_stats_utils_tables.R
@@ -10,17 +10,127 @@ render_datatable <- function(data, out_filename = "data", selection = "none", ..
       filter = FALSE,
       lengthChange = FALSE,
       scrollX = TRUE,
-      dom = "tBp",
+      dom = "
+      <'row'<'col-sm-12'tr>>
+      <'row'<'col-sm-12 col-md-7'pB>>
+      ",
       buttons = list(
         list(
-          extend = "csv",
-          filename = out_filename
-        ),
-        list(
-          extend = "excel",
-          filename = out_filename
+          extend = "collection",
+          buttons = list(
+            list(extend = "csv", filename = out_filename),
+            list(extend = "excel", filename = out_filename)
+          ),
+          text = "<i class='fa fa-download' role='presentation' aria-label='download icon'></i> Download"
         )
       )
+      # dom = "tBp",
+      # buttons = list(
+      #   list(
+      #     extend = "csv",
+      #     filename = out_filename
+      #   ),
+      #   list(
+      #     extend = "excel",
+      #     filename = out_filename
+      #   )
+      # )
+    ),
+    ...
+  )
+
+  return(table)
+}
+
+render_datatable_justified <- function(data, out_filename = "data", selection = "none", ...) {
+  table <- DT::datatable(
+    data = data,
+    style = "bootstrap4",
+    rownames = FALSE,
+    selection = selection,
+    extensions = "Buttons",
+    options = list(
+      pageLength = 10,
+      filter = FALSE,
+      lengthChange = FALSE,
+      scrollX = TRUE,
+      columnDefs = list(
+        # list(width = "100px", targets = c(0)),
+        list(className = 'dt-center', targets = c(0, 1))
+      ),
+      dom = "
+      <'row'<'col-sm-12'tr>>
+      <'row'<'col-sm-12 col-md-7'pB><'col-sm-12 col-md-5 text-right'i>>
+      ",
+      buttons = list(
+        list(
+          extend = "collection",
+          buttons = list(
+            list(extend = "csv", filename = out_filename),
+            list(extend = "excel", filename = out_filename)
+          ),
+          text = "<i class='fa fa-download' role='presentation' aria-label='download icon'></i> Download"
+        )
+      )
+      # dom = "tBp",
+      # buttons = list(
+      #   list(
+      #     extend = "csv",
+      #     filename = out_filename
+      #   ),
+      #   list(
+      #     extend = "excel",
+      #     filename = out_filename
+      #   )
+      # )
+    ),
+    ...
+  )
+
+  return(table)
+}
+
+render_datatable_justified_nopage <- function(data, out_filename = "data", selection = "none", ...) {
+  table <- DT::datatable(
+    data = data,
+    style = "bootstrap4",
+    rownames = FALSE,
+    selection = selection,
+    extensions = "Buttons",
+    options = list(
+      pageLength = 10,
+      filter = FALSE,
+      lengthChange = FALSE,
+      scrollX = TRUE,
+      columnDefs = list(
+        # list(width = "100px", targets = c(0)),
+        list(className = 'dt-center', targets = c(0, 1))
+      ),
+      dom = "
+      <'row'<'col-sm-12'tr>>
+      <'row'<'col-sm-12 col-md-7'B><'col-sm-12 col-md-5 text-right'i>>
+      ",
+      buttons = list(
+        list(
+          extend = "collection",
+          buttons = list(
+            list(extend = "csv", filename = out_filename),
+            list(extend = "excel", filename = out_filename)
+          ),
+          text = "<i class='fa fa-download' role='presentation' aria-label='download icon'></i> Download"
+        )
+      )
+      # dom = "tBp",
+      # buttons = list(
+      #   list(
+      #     extend = "csv",
+      #     filename = out_filename
+      #   ),
+      #   list(
+      #     extend = "excel",
+      #     filename = out_filename
+      #   )
+      # )
     ),
     ...
   )
