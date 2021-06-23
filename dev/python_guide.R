ff767c51bc73d6538bea077e760eb65fcc7c4869
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-06-23 13:53:03 +0100

    Updated virtualenv setup instructions in python_guide.R

--- dev/python_guide.R
+++ dev/python_guide.R
@@ -1,11 +1,12 @@
 # Setup ----
 usethis::use_package("reticulate")
 
+# cd dev
 # curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # python3 get-pip.py
 
-# pip install virtualenv
-# pip install numpy
+# python3 -m pip install virtualenv
+# python3 -m pip install numpy
 
 usethis::edit_r_profile(scope = "project")
 # Copy https://raw.githubusercontent.com/ranikay/shiny-reticulate-app/master/.Rprofile
@@ -15,13 +16,20 @@ usethis::edit_r_profile(scope = "project")
 # Debug using
 reticulate::py_discover_config()
 
-reticulate::virtualenv_create(envname = "shiny_nanorod_env", python = "/usr/local/bin/python3")
+# Create virtualenv
+virtualenv_dir <- Sys.getenv("VIRTUALENV_NAME")
+python_path <- Sys.getenv("PYTHON_PATH")
+
+reticulate::use_python(python_path, required = TRUE)
+reticulate::virtualenv_create(envname = virtualenv_dir, python = python_path)
+reticulate::use_virtualenv(virtualenv_dir, required = TRUE)
+
+# Install dependencies
+PYTHON_DEPENDENCIES <- c("ncempy", "scikit-image", "scipy")
+reticulate::virtualenv_install(virtualenv_dir, packages = PYTHON_DEPENDENCIES, ignore_installed = TRUE)
 
 # Move python code to /src
 
 
 # Run on terminal ----
 # source ~/.virtualenvs/shiny_nanorod_env/bin/activate
-
-# Dependencies
-c("ncempy", "scikit-image", "scipy")
