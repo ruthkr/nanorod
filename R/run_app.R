03f1075cc5f3dcbc5395e852cc15ab0340749b17
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-08-23 22:09:56 +0100

    Added documentation for display.mode and launch.browser parameters on run_app() function

--- R/run_app.R
+++ R/run_app.R
@@ -1,5 +1,7 @@
 #' Run the Shiny Application
 #'
+#' @param display.mode The mode in which to display the application. If set to the value \code{"showcase"}, shows application code and metadata from a \code{DESCRIPTION} file in the application directory alongside the application. Defaults to \code{"normal"}, which displays the application normally.
+#' @param launch.browser If true, the system's default web browser will be launched automatically after the app is started. Defaults to true in interactive sessions only. This value of this parameter can also be a function to call with the application's URL.
 #' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
